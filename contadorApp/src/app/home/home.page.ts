27809f0e110e3d1bcb49626be48264c33cff694d
Nicolas Miranda <nicolas.miranda.ibarra.1999@gmail.com>
2024-12-03 18:32:32 -0300

    Se agrega botones y funciones para el contador de numeros

--- contadorApp/src/app/home/home.page.ts
+++ contadorApp/src/app/home/home.page.ts
@@ -7,6 +7,35 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  // Declaracion de variables
+  public numero: number;
+  public numCero: string;
+
+  constructor() {
+    // Llamada al constructor y dando su valor
+    this.numero = 0;
+    this.numCero = '00'
+  }
+
+  // Funcion
+  subirNumero(){
+    this.numero++;
+    this.validacionNum();
+  }
+
+  // Funcion
+  bajarNumero(){
+    this.numero--;
+    this.validacionNum();
+  }
+
+  // Validacion
+  validacionNum(){
+    if(this.numero < 10) {
+      this.numCero = '0' + this.numero;
+    } else {
+      this.numCero = '' + this.numero;
+    }
+  }
 
 }
